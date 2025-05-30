77ee66ebcfe2a5725b19e059b8d904222a7d0ff1
ViniciusFrancaCroa46 <viniciusfrancacoviello@gmail.com>
2025-05-30 18:28:01 -0300

    Correção: Adição do WebConfig & Atualização da class Barreira

--- src/main/java/com/fiap/gs/api_cereus/model/Barreira.java
+++ src/main/java/com/fiap/gs/api_cereus/model/Barreira.java
@@ -1,32 +1,37 @@
 package com.fiap.gs.api_cereus.model;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "Barreiras")
 public class Barreira {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private boolean funcionamento;  // Está funcionando corretamente?
-    private boolean ativacao;       // Está ativada?
+    private boolean ativacao;
+    private LocalDateTime dataHoraAtivacao;
+
+    @ManyToOne
+    @JoinColumn(name = "sensor_agua_id", nullable = false)
+    private SensorAgua sensorAgua;
 
     //Construtores
     public Barreira() {}
 
-    public Barreira(boolean funcionamento, boolean ativacao) {
-        this.funcionamento = funcionamento;
+    public Barreira(boolean ativacao, LocalDateTime dataHoraAtivacao, SensorAgua sensorAgua) {
         this.ativacao = ativacao;
+        this.dataHoraAtivacao = dataHoraAtivacao;
+        this.sensorAgua = sensorAgua;
     }
 
     // Getters e Setters
     public Long getId() {return id;}
 
-    public boolean isFuncionamento() {return funcionamento;}
-    public void setFuncionamento(boolean funcionamento) {this.funcionamento = funcionamento;}
-
     public boolean isAtivacao() {return ativacao;}
     public void setAtivacao(boolean ativacao) {this.ativacao = ativacao;}
+
+    public LocalDateTime getDataHoraAtivacao() { return dataHoraAtivacao; }
+    public void setDataHoraAtivacao(LocalDateTime dataHoraAtivacao) { this.dataHoraAtivacao = dataHoraAtivacao; }
 }
